8add667220e6b4d8a7033cc1c5746362acb1dac6
Ryan Shin <ryans@apisplatform.io>
2018-08-28 10:06:30 +0900

    Connected token interface

--- contract/BlockSurvey.sol
+++ contract/BlockSurvey.sol
@@ -1,6 +1,172 @@
 pragma solidity ^0.4.23;
 pragma experimental ABIEncoderV2;
 
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+
+contract SurveyToken {
+    // Public variables of the token
+    string public name = "SurveyToken";
+    string public symbol = "SVT";
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply = 100000000000000000000000000000;
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    // This generates a public event on the blockchain that will notify clients
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+    }
+
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function multiTransferFrom(address _from, address[] _to, uint256[] _value) public returns (bool success) {
+        uint256 sum;
+        for(uint i = 0; i < _to.length; i++) {
+            sum += _value[i];
+        }
+        
+        require(sum <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= sum;
+
+        for(i = 0; i < _to.length; i++) {
+            _transfer(_from, _to[i], _value[i]);
+        }
+        
+        return true;
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
+}
+
 contract BlockSurvey{
 
     event createdPoll(address creater, uint256 pollid);
@@ -9,7 +175,7 @@ contract BlockSurvey{
 
     // Poll Part
     struct Poll{
-        address creator;      
+        address creator; 
         uint256 pollID;
 
         uint256 startTime;
@@ -20,7 +186,9 @@ contract BlockSurvey{
         bool isFinished;
         string questionSheet;
 
-        mapping(address => uint256) participant; // 누구든지 확인할 수 있음. 해당 참여자의 참여 여부 확인 가능
+        uint256 deposit;
+
+        mapping(uint256 => address) participant; // 누구든지 확인할 수 있음. 해당 참여자의 참여 여부 확인 가능
         mapping(uint256 => string) answerSheet;  // 접근 권한 제한 필요. 개발자만 제한적으로 조회할 수 있는 함수 개발 예정.
     }
 
@@ -38,6 +206,8 @@ contract BlockSurvey{
     uint256 private pollCount = 0;
     uint256 private userCount = 0;
 
+    SurveyToken svt = SurveyToken(0xEF1F53c07a345Cc120531853Ed8A07399c5dC773);
+
 
     modifier pollJoinLimitReached(uint pollID) {
         if (pollList[pollID].answerCount >= pollList[pollID].answerLimit) revert("Poll join limit reached!");
@@ -51,20 +221,48 @@ contract BlockSurvey{
         if (pollList[pollID].isFinished) revert("Poll is not alive!");
         _;
     }
-    modifier pollJoined(uint pollID) {
-        if (pollList[pollID].participant[msg.sender] < 0) revert("Poll is not alive!");
+    modifier pollJoined(uint pollID, address userAddress) {
+        for(uint i = 0; i < pollList[pollID].answerCount; i++)
+        {
+            if (pollList[pollID].participant[i] == userAddress) _;
+        }
+        revert("User not joined!");
+    }
+    modifier pollOwner(uint pollID) {
+        if (pollList[pollID].creator != msg.sender) revert("Not owner!");
         _;
     }
+
     modifier userJoined(address userAddress) {
-        if (userList[userAddress].userID < 0) revert("User arleady joined!");
+        if(userList[userAddress].userID < 0) revert("User not joined!");
         _;
     }
+
     modifier adminOnly() {
         // if (msg.sender != admin) revert("not admin");
         if (msg.sender != msg.sender) revert("User is not admin!");
         _;
     }
 
+
+    
+    /**
+    * Token part
+    * - in this part, can connect to SurveyToken contract which is arleady deployed
+    * - can be used in payment progress
+    */
+
+    function depositToken(address _from, uint _value)
+        internal
+    {
+        svt.transferFrom(_from, 0x8cad9b4941aafb67b5a5e6dea657db2d4ea7b757, _value);
+    }
+
+    function sendToken(address[] _to, uint[] _value)
+        internal
+    {
+        svt.multiTransferFrom(0x8cad9b4941aafb67b5a5e6dea657db2d4ea7b757, _to, _value);
+    }
 
 
     /**
@@ -107,14 +305,15 @@ contract BlockSurvey{
     * - joining logic is merged to createAnswer method
     */
 
-    function createPoll(uint256 answerLimit, uint256 timeLimit, string questionSheet)
+    function createPoll(uint256 answerLimit, uint256 timeLimit, string questionSheet, uint256 fee)
         public
         payable
         returns(uint256 pollID) 
     {
         //Poll(msg.sender, pollCount, block.timestamp, (block.timestamp + timeLimit), answerLimit, 0, false, questionSheet);
+        depositToken(msg.sender, fee);
 
-        pollList[pollCount] = (Poll(msg.sender, pollCount, block.timestamp, (block.timestamp + timeLimit), answerLimit, 0, false, questionSheet));
+        pollList[pollCount] = (Poll(msg.sender, pollCount, block.timestamp, (block.timestamp + timeLimit), answerLimit, 0, false, questionSheet, fee));
         pollID = pollCount++;
     }
 
@@ -137,6 +336,27 @@ contract BlockSurvey{
         answerLimit = pollList[pollID].answerLimit;
         answerCount = pollList[pollID].answerCount;
         questionSheet = pollList[pollID].questionSheet;
+    }
+
+    function finishPoll(uint256 pollID)
+        public
+        view
+
+        pollStillAlive(pollID)
+        pollOwner(pollID)
+
+        returns(bool isSuccessed)
+    {
+        address[] receivers;
+        uint256[] values;
+
+        for(uint i = 0; i < pollList[pollID].answerCount; i++)
+        {
+            receivers.push(pollList[pollID].participant[i]);
+            values.push(pollList[pollID].deposit / pollList[pollID].answerCount);
+        }
+        sendToken(receivers, values);
+        pollList[pollID].isFinished = true;
     }
 
     function getQuestion(uint256 pollID)
@@ -166,9 +386,9 @@ contract BlockSurvey{
         
         returns(bool result, uint answerID)
     {
-        pollList[pollID].participant[msg.sender] = answerID;
-
+        pollList[pollID].participant[pollList[pollID].answerCount] = msg.sender;
         pollList[pollID].answerSheet[pollList[pollID].answerCount] = answer;
+
         answerID = pollList[pollID].answerCount;
         result = true;
 
