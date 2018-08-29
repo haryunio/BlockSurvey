7944c2e6beea993c9f7e607e70b703426b55b06e
Ryan Shin <ryans@apisplatform.io>
2018-08-29 14:47:46 +0900

    Code refactor

--- contract/BlockSurveyAIO.sol
+++ contract/BlockSurveyAIO.sol
@@ -4,78 +4,44 @@ pragma experimental ABIEncoderV2;
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
 contract BlockSurveyAIO {
-    // Public variables of the token
-    string public name = "SurveyToken";
-    string public symbol = "SVT";
+    string public name = "SurveyTokenBeta";
+    string public symbol = "SVTb";
     uint8 public decimals = 18;
-    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply = 100000000000000000000000000000;
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    // This generates a public event on the blockchain that will notify clients
+    // token event
     event Transfer(address indexed from, address indexed to, uint256 value);
-    
-    // This generates a public event on the blockchain that will notify clients
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
+    // blocksurvey event
+    event createdPoll(address creater, uint256 pollid);
+    event JoinedPoll(address user, uint256 pollid, uint256 time);
+    event createdAnswer(address user, uint256 pollid, uint256 answerid);
 
-    /**
-     * Constructor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
     constructor() public {
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
     }
 
-    /**
-     * Internal transfer, only can be called by this contract
-     */
     function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
-        // Check if the sender has enough
         require(balanceOf[_from] >= _value);
-        // Check for overflows
         require(balanceOf[_to] + _value >= balanceOf[_to]);
-        // Save this for an assertion in the future
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
         balanceOf[_from] -= _value;
-        // Add the same to the recipient
         balanceOf[_to] += _value;
         emit Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
     function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
         return true;
     }
 
-    /**
-     * Transfer tokens from other address
-     *
-     * Send `_value` tokens to `_to` on behalf of `_from`
-     *
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
@@ -83,14 +49,6 @@ contract BlockSurveyAIO {
         return true;
     }
 
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     */
     function approve(address _spender, uint256 _value) public
         returns (bool success) {
         allowance[msg.sender][_spender] = _value;
@@ -98,15 +56,6 @@ contract BlockSurveyAIO {
         return true;
     }
 
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData)
         public
         returns (bool success) {
@@ -117,13 +66,6 @@ contract BlockSurveyAIO {
         }
     }
 
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
     function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
         balanceOf[msg.sender] -= _value;            // Subtract from the sender
@@ -132,14 +74,6 @@ contract BlockSurveyAIO {
         return true;
     }
 
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
         require(_value <= allowance[_from][msg.sender]);    // Check allowance
@@ -150,11 +84,8 @@ contract BlockSurveyAIO {
         return true;
     }
 
-    event createdPoll(address creater, uint256 pollid);
-    event JoinedPoll(address user, uint256 pollid, uint256 time);
-    event createdAnswer(address user, uint256 pollid, uint256 answerid);
+    
 
-    // Poll Part
     struct Poll{
         address creator; 
         uint256 pollID;
@@ -227,8 +158,6 @@ contract BlockSurveyAIO {
         _;
     }
 
-
-    
     /**
     * Token part
     * - in this part, can connect to SurveyToken contract which is arleady deployed
